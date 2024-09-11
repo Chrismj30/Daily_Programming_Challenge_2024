b02b54552075151833fa116dc086ffdc7470375c
Chrismj30 <161944359+Chrismj30@users.noreply.github.com>
2024-09-11 14:38:17 +0530

    Update DuplicateN.cpp

--- DuplicateN.cpp
+++ DuplicateN.cpp
@@ -9,7 +9,7 @@ public:
         do {
             slow=nums[slow];
             fast=nums[nums[fast]];
-        } while(slow != fast);
+        } while(slow!=fast);
         slow=nums[0];
         while(slow!=fast) {
             slow=nums[slow];
