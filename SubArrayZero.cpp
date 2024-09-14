3ade0f864d7dac125df7df5c646e146c71676601
Chrismj30 <161944359+Chrismj30@users.noreply.github.com>
2024-09-14 23:29:38 +0530

    Update SubArrayZero.cpp

--- SubArrayZero.cpp
+++ SubArrayZero.cpp
@@ -6,28 +6,27 @@ public:
     vector<vector<int>> subarray(vector<int>& nums) {
         unordered_map<int, vector<int>> map;  
         vector<vector<int>> ans;
-        int sum = 0;
+        int sum=0;
         map[0].push_back(-1);
         for (int i = 0; i < nums.size(); i++) {
-            sum += nums[i];  
+            sum+=nums[i];  
             if (map.find(sum) != map.end()) {
-                for (auto it : map[sum]) {
+                for(auto it:map[sum]){
                     ans.push_back({it+1,i});
                 }
             }
             map[sum].push_back(i);
         }
-
         return ans;
     }
 };
 
 int main() {
     Solution s;
-    vector<int> nums = {1, 2, -3, 3, -1, 2};
-    vector<vector<int>> result = s.subarray(nums);
+    vector<int> nums={1, 2, -3, 3, -1, 2};
+    vector<vector<int>> result=s.subarray(nums);
 
-    for (auto it : result) {
-        cout << "[" << it[0] << "," << it[1] << "] ";
+    for (auto it:result) {
+        cout<<"["<<it[0]<<","<<it[1]<<"] ";
     }
 }
